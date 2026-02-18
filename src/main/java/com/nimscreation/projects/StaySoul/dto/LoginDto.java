03291e7423bf2e3bdd5651e0a2f8d0319a6c895f
Nims-Creation <nimscreation06@gmail.com>
2026-02-18 18:14:37 +0530

    Loginflow service is completed

---
+++ src/main/java/com/nimscreation/projects/StaySoul/dto/LoginDto.java
@@ -0,0 +1,11 @@
+package com.nimscreation.projects.StaySoul.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+
+    private String email;
+    private String userName;
+    private String password;
+}
--- src/main/java/com/nimscreation/projects/StaySoul/security/AuthService.java
+++ src/main/java/com/nimscreation/projects/StaySoul/security/AuthService.java
@@ -1,5 +1,6 @@
 package com.nimscreation.projects.StaySoul.security;
 
+import com.nimscreation.projects.StaySoul.dto.LoginDto;
 import com.nimscreation.projects.StaySoul.dto.SignUpRequestDto;
 import com.nimscreation.projects.StaySoul.dto.UserDto;
 import com.nimscreation.projects.StaySoul.entity.User;
@@ -7,6 +8,9 @@ import com.nimscreation.projects.StaySoul.entity.enums.Roles;
 import com.nimscreation.projects.StaySoul.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +23,8 @@ public class AuthService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JWTService jwtService;
 
     public UserDto  signUp(SignUpRequestDto signUpRequestDto ){
         User user = userRepository.findByEmail(signUpRequestDto.getEmail()).orElse(null);
@@ -35,6 +41,20 @@ public class AuthService {
 
         return modelMapper.map(newUser, UserDto.class);
 
+    }
+
+    public String[] login(LoginDto loginDto){
+        Authentication authentication =  authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginDto.getEmail(), loginDto.getPassword()
+        ));
+
+        User user = (User) authentication.getPrincipal();
+
+        String[] arr= new String[2];
+        arr[0] = jwtService.generateAccessToken(user);
+        arr[1] = jwtService.generateRefreshToken(user);
+
+        return arr;
     }
 }
 
